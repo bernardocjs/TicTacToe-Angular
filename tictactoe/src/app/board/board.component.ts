ba1a70cda8d83a14992f44df625f04b71db124a7
Bernardo <engenharia16@tkth.com.br>
2023-04-12 16:04:01 -0300

    feat: :sparkles: adds playable tic tac toe

--- tictactoe/src/app/board/board.component.ts
+++ tictactoe/src/app/board/board.component.ts
@@ -3,12 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
-  styleUrls: ['./board.component.css']
+  styleUrls: ['./board.component.scss']
 })
 export class BoardComponent implements OnInit {
   squares: any[] = [];
   xIsNext: boolean = false;
-  winner!: string ;
+  winner!: string | null ;
 
     ngOnInit(): void {
     this.newGame();
@@ -16,7 +16,7 @@ export class BoardComponent implements OnInit {
 
   newGame() {
     this.squares = Array(9).fill(null);
-    this.winner = '';
+    this.winner = null;
     this.xIsNext = true;
   }
 
@@ -24,7 +24,36 @@ export class BoardComponent implements OnInit {
     return this.xIsNext ? 'X' : 'O';
   }
 
-  playerMove(){
-    this.xIsNext = !this.xIsNext;
+  playerMove(i: number){
+    if(!this.squares[i]){
+      this.squares.splice(i, 1, this.player) //substitui no index clicado com o player
+      this.xIsNext = !this.xIsNext;
+
+      this.winner = this.calculateWinner();
+    }
+
+  }
+
+  calculateWinner() {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (this.squares[a] && this.squares[a] === this.squares[b] && this.squares[a] === this.squares[c]) {
+        return this.squares[a];
+      }
+    }
+    if(!this.squares.includes(null))
+      return 'Ninguem';
+
+
   }
 }
--- tictactoe/src/app/square/square.component.ts
+++ tictactoe/src/app/square/square.component.ts
@@ -3,12 +3,20 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-square',
   template: `
-  <button>{{value}}</button>`,
+   <button nbButton *ngIf="value">{{ value }}</button>
+   <!-- <button nbButton *ngIf="value=='X'" status="success">X</button>
+   <button nbButton *ngIf="value=='O'" status="info">X</button> -->
+   `,
   styles: [
+    'button { width: 100%; height: 100%; font-color: black; font-size: 5em !important; }'
   ]
 })
 export class SquareComponent {
 
   @Input() value: 'X' | 'O' | undefined;
+
+  chooseColor() {
+    
+  }
 
 }
