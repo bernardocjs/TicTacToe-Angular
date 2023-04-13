a914b4564872faf86362582ec80ad4e9e475ac7c
Bernardo <engenharia16@tkth.com.br>
2023-04-13 09:53:23 -0300

    refactor: :ambulance: fixes angular cli error

--- TicTacToe/src/app/square/square.component.ts
+++ TicTacToe/src/app/square/square.component.ts
@@ -3,9 +3,9 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-square',
   template: `
-   <button nbButton *ngIf="value">{{ value }}</button>
-   <!-- <button nbButton *ngIf="value=='X'" status="success">X</button>
-   <button nbButton *ngIf="value=='O'" status="info">X</button> -->
+   <button nbButton *ngIf="!value">{{ value }}</button>
+   <button nbButton status="success" *ngIf="value=='X'" >X</button>
+   <button nbButton status="info" *ngIf="value=='O'" >O</button>
    `,
   styles: [
     'button { width: 100%; height: 100%; font-color: black; font-size: 5em !important; }'
@@ -16,7 +16,7 @@ export class SquareComponent {
   @Input() value: 'X' | 'O' | undefined;
 
   chooseColor() {
-    
+
   }
 
 }
