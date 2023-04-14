0e967c326e0f1003763f22a799f0fe2899859703
Bernardo <engenharia16@tkth.com.br>
2023-04-14 12:05:40 -0300

    feat: :sparkles: adds socket connection in rooms

--- BackEnd/src/sockets/sockets.gateway.ts
+++ BackEnd/src/sockets/sockets.gateway.ts
@@ -9,17 +9,22 @@ import {
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { SocketService } from './sockets.service';
+import { IPlayer } from './IPlayer';
+import { ISession } from './ISession';
 
 @WebSocketGateway()
 export class SocketsGateway implements OnGatewayInit, OnModuleDestroy {
   constructor(@Inject(SocketService) private socketService: SocketService) {}
-  players: [] = [];
+  players: IPlayer[] = [];
+  sessions: ISession[] = [];
 
   @WebSocketServer()
   server: Server;
 
   afterInit(server: Server) {
     this.socketService.socket = server;
+    this.socketService.players = this.players;
+    this.socketService.sessions = this.sessions;
   }
 
   onModuleDestroy() {
@@ -27,10 +32,45 @@ export class SocketsGateway implements OnGatewayInit, OnModuleDestroy {
   }
 
   async handleConection(client: Socket) {
-    await client.join(client.handshake.query.sessionId.toString());
+    const player = client.handshake.query as unknown as IPlayer;
+
+    const playerInGame = this.players.some((p) => p.id === player.id);
+
+    if (playerInGame) {
+      client.disconnect();
+      return;
+    }
+
+    const sessionExists = this.sessions.filter((s) => s.id === player.session);
+
+    if (sessionExists) {
+      if (sessionExists[0].players.length >= 2) {
+        client.disconnect();
+        return;
+      }
+      sessionExists[0].players.push(player);
+
+      const sessionIndex = this.sessions.findIndex(
+        (s) => s.id === sessionExists[0].id,
+      );
+      this.sessions.splice(sessionIndex, 1, sessionExists[0]);
+    } else {
+      this.sessions.push({ id: player.session, players: [player] });
+    }
+
+    this.players.push(player);
+
+    await client.join(player.session);
   }
 
   async handleDisconnect(client: Socket) {
     await client.leave(client.handshake.query.sessionId.toString());
+  }
+
+  async handleChange(client: Socket, payload: string[]) {
+    //emit array received
+    client
+      .to(client.handshake.query.sessionId.toString())
+      .emit('change', payload);
   }
 }
--- BackEnd/src/sockets/sockets.service.ts
+++ BackEnd/src/sockets/sockets.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { Server } from 'socket.io';
+import { IPlayer } from './IPlayer';
+import { ISession } from './ISession';
 
 @Injectable()
 export class SocketService {
   public socket: Server = null;
+  public players: IPlayer[] = [];
+  public sessions: ISession[] = [];
 }
